fe1dc2612cd2b9de68fd8de74237cfbd2e2be981
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-09-28 22:16:21 -0300

    Adição de Função de apagar e alterar

--- src/controller/APISanBolosController.js
+++ src/controller/APISanBolosController.js
@@ -17,6 +17,61 @@ endpoint.post('/inserirProduto/', async (req, resp) => {
   }
 });
 
+endpoint.get('/consultarProduto/', async (req, resp) => {
+  try {
+    let registros = await db.consultarProduto();
+    resp.send(registros);
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+endpoint.put('/alterarProduto/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let produto = req.body;
+    let lineaffect = await db.alterarProduto(id, produto);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+endpoint.delete('/removerProduto/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+
+    let lineaffect = await db.removerProduto(id);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+
+
 export default endpoint;
 
 
--- src/repository/APISanBolosRepository.js
+++ src/repository/APISanBolosRepository.js
@@ -8,9 +8,9 @@ export async function inserirProduto(produto) {
 
   let resposta = await con.query(comando, [
     produto.id,
-    produto.nome, 
-    produto.descrição, 
-    produto.preço,     
+    produto.nome,
+    produto.descrição,
+    produto.preço,
     produto.image,
     produto.disponibilidade
   ]);
@@ -18,4 +18,56 @@ export async function inserirProduto(produto) {
   let info = resposta[0];
 
   return info.insertId;
+}
+
+export async function consultarProduto() {
+  const comando = `
+    select id_produto         as id,
+          nm_produto          as nome,
+          ds_descrição        as descrição,
+          vl_preço            as valor,
+          img_produto         as image,
+          bl_disponibilidade  as disponivel
+    from tb_produto;
+  `;
+  let resposta = await con.query(comando);
+  let registro = resposta[0];
+
+  return registro;
+}
+
+export async function alterarProduto(id, produto) {
+  const comando = `
+    update tb_produto
+    set nm_produto = ?,
+        ds_descrição = ?,
+        vl_preço = ?,
+        img_produto = ?,
+        bl_disponibilidade = ?
+    where id_produto = ?;
+  `;
+  let resposta = await con.query(comando, [
+    produto.id,
+    produto.nome,
+    produto.descrição,
+    produto.preço,
+    produto.image,
+    produto.disponibilidade,
+    id,
+  ]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+
+};
+
+export async function removerProduto(id) {
+  const comando = `
+    delete from tb_produto
+    where id_produto = ?;
+  `;
+  let resposta = await con.query(comando, [id]);
+  let info = resposta[0];
+
+  return info.affectedRows;
 }
