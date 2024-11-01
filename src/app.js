6b4ffc5f400899525ddc097aa6da46d576a7262e
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-11-01 10:31:23 -0300

    Arrumando 10h31

--- src/app.js
+++ src/app.js
@@ -1,29 +1,48 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import adicionarRotas from './rotas.js';
-import con from './repository/connection.js';
+import express from 'express'
+import adicionarRotas from './rotas.js'
+import cors from 'cors'
+import 'dotenv/config'
 
-const server = express();
-server.use(cors());
-server.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
+adicionarRotas(app)
 
-adicionarRotas(server);
 
-server.get('/Produto', async (req, res) => {
-  try {
-    const connection = await con();
-    const [rows] = await connection.query('SELECT 1 AS test');
-    res.send({ message: 'Conexão com o banco de dados bem-sucedida!', resultado: rows });
-  } catch (err) {
-    res.status(500).send({ erro: 'Falha na conexão com o banco de dados', detalhe: err.message });
-  }
-});
 
-const PORTA = process.env.PORTA;
-console.log(PORTA);
+const porta = process.env.API_PORTA
+app.listen(porta, ()=>{
+    console.log('aberto na porta ' + process.env.PORTA)
+})
 
-server.listen(PORTA, () => {
-  console.log(`API SUBIU na porta ${PORTA}`);
-});
+
+
+// import 'dotenv/config';
+// import express from 'express';
+// import cors from 'cors';
+// import adicionarRotas from './rotas.js';
+// import con from './repository/connection.js';
+
+// const server = express();
+// server.use(cors());
+// server.use(express.json());
+
+// adicionarRotas(server);
+
+// server.get('/Produto', async (req, res) => {
+//   try {
+//     const connection = await con();
+//     const [rows] = await connection.query('SELECT 1 AS test');
+//     res.send({ message: 'Conexão com o banco de dados bem-sucedida!', resultado: rows });
+//   } catch (err) {
+//     res.status(500).send({ erro: 'Falha na conexão com o banco de dados', detalhe: err.message });
+//   }
+// });
+
+// const PORTA = process.env.PORTA;
+// console.log(PORTA);
+
+// server.listen(PORTA, () => {
+//   console.log(`API SUBIU na porta ${PORTA}`);
+// });
 
