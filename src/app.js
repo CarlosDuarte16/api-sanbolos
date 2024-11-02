48207ec31230cb8e2a60b73444903cdd3b613309
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-11-01 23:19:14 -0300

    Funcional para server do brunão

--- src/app.js
+++ src/app.js
@@ -1,48 +1,29 @@
-import express from 'express'
-import adicionarRotas from './rotas.js'
-import cors from 'cors'
-import 'dotenv/config'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import adicionarRotas from './rotas.js';
+import con from './repository/connection.js';
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-adicionarRotas(app)
+const server = express();
+server.use(cors());
+server.use(express.json());
 
+adicionarRotas(server);
 
+server.get('/Produto', async (req, res) => {
+  try {
+    const connection = await con();
+    const [rows] = await connection.query('SELECT 1 AS test');
+    res.send({ message: 'Conexão com o banco de dados bem-sucedida!', resultado: rows });
+  } catch (err) {
+    res.status(500).send({ erro: 'Falha na conexão com o banco de dados', detalhe: err.message });
+  }
+});
 
-const porta = process.env.PORTA || 30000
-app.listen(porta, ()=>{
-    console.log('aberto na porta ' + process.env.PORTA)
-})
+const PORTA = process.env.PORTA;
+console.log(PORTA);
 
-
-
-// import 'dotenv/config';
-// import express from 'express';
-// import cors from 'cors';
-// import adicionarRotas from './rotas.js';
-// import con from './repository/connection.js';
-
-// const server = express();
-// server.use(cors());
-// server.use(express.json());
-
-// adicionarRotas(server);
-
-// server.get('/Produto', async (req, res) => {
-//   try {
-//     const connection = await con();
-//     const [rows] = await connection.query('SELECT 1 AS test');
-//     res.send({ message: 'Conexão com o banco de dados bem-sucedida!', resultado: rows });
-//   } catch (err) {
-//     res.status(500).send({ erro: 'Falha na conexão com o banco de dados', detalhe: err.message });
-//   }
-// });
-
-// const PORTA = process.env.PORTA;
-// console.log(PORTA);
-
-// server.listen(PORTA, () => {
-//   console.log(`API SUBIU na porta ${PORTA}`);
-// });
+server.listen(PORTA, () => {
+  console.log(`API SUBIU na porta ${PORTA}`);
+});
 
--- src/repository/connection.js
+++ src/repository/connection.js
@@ -1,13 +1,25 @@
-import mysql from 'mysql2/promise'
-import 'dotenv/config.js'
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
 
-const con =  await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DB,
-    password: process.env.MYSQL_PWD,
-    user: process.env.MYSQL_USER,
-    port: process.env.PORTA
+const con = await mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+  typeCast: function (field, next) {
+
+    if (field.type === "TINY" && field.length === 1) {
+      return (field.string() === "1")
+    } else if (field.type.includes("DECIMAL")) {
+      return Number(field.string())
+    } else {
+      return next()
+    }
+  }
 })
-console.log('conectado') 
+console.log("Host:", process.env.MYSQL_HOST);
+console.log("Usuário:", process.env.MYSQL_USER);
+console.log("Banco de Dados:", process.env.MYSQL_DB);
 
-export default con    
+console.log("Db conectado")
+export default con;
